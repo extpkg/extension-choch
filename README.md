2e4cefa798bcbe11fa769c4f0780720f70a6e4b4
Bodgan B Peterson <github@bogdanbpeterson.com>
2023-10-26 18:20:09 +0300

    feat: converts to the right religion, uh, oops, I mean, build system

--- README.md
+++ README.md
@@ -1,28 +1,15 @@
-# CHOCH Extension
+# CHOCH
 
----
+CHOCH: 404 Game Not Found
 
-Steps to re-create this extension:
+## Build
 
-1. Download https://github.com/kostik1337/CHOCH
+1. Clone the repository
+2. Run `npm install` to install node dependencies
+3. Run `npm run build` to build the extension
+4. Copy the `dist` directory into EXT
 
-2. Run npm install to download all dependencies
+## Development
 
-3. Install gulp with npm install -g gulp-cli
-
-4. Download shader minifier 1.1.6 from https://github.com/laurentlb/Shader_Minifier/releases **Note:** The latest version doesn't work
-
-5. Set SHADER_MINIFIER_CMD environment variable to the shader minifier executable
-
-6. Update src/js/main.js line 54 to run main function after a timeout of 100 milliseconds. This fixes the "texImage2D: no canvas" error in setTextureCanvasData which made the menu invisible
-
-7. Add the following line in src/glsl/f_shader.glsl after line 441:
-
-```javascript
-   if (speed.x < 0.001 && speed.x > -0.001 && speed.y < 0.001 && speed.y > -0.001) spiderAngle = PI/2.;
-   // This fixes the MacOS bug where the screen went black when the player speed was zero (on death or on startup)
-```
-
-8. Run gulp build_release and copy the index.html to extension directory
-
-9. Extension manifest, code & zip file to load the index.html
+Run `npm run dev` to watch `src` and `public` directories and rebuild the
+project on changes
---
+++ src/ext/main.ts
@@ -0,0 +1,169 @@
+type Instance = {
+  tabId: string;
+  windowId: string;
+  webviewId: string;
+  websessionId: string;
+};
+
+let instance: Instance | null = null;
+
+const title = "CHOCH";
+
+const focusInstance = async () => {
+  if (instance) {
+    await ext.windows.restore(instance.windowId);
+    await ext.windows.focus(instance.windowId);
+  }
+};
+
+const destroyInstance = async () => {
+  if (instance) {
+    await ext.windows.remove(instance.windowId);
+    await ext.tabs.remove(instance.tabId);
+    await ext.webviews.remove(instance.webviewId);
+    await ext.websessions.remove(instance.websessionId);
+    instance = null;
+  }
+};
+
+ext.runtime.onExtensionClick.addListener(async () => {
+  if (instance) {
+    await focusInstance();
+    return;
+  }
+
+  let webview: ext.webviews.Webview | null = null;
+  let websession: ext.websessions.Websession | null = null;
+  let window: ext.windows.Window | null = null;
+  let tab: ext.tabs.Tab | null = null;
+
+  try {
+    tab = await ext.tabs.create({
+      text: title,
+      icon: "./assets/128.png",
+      mutable: true,
+    });
+
+    const aspectRatio = 650 / 460;
+    const minWidth = 960;
+    const minHeight = minWidth / aspectRatio;
+
+    window = await ext.windows.create({
+      center: true,
+      fullscreenable: true,
+      title,
+      icon: "./assets/128.png",
+      vibrancy: false,
+      frame: false,
+      titleBarStyle: "inset",
+      width: minWidth,
+      height: minHeight,
+      minWidth,
+      minHeight,
+      aspectRatio,
+    });
+
+    const contentSize = await ext.windows.getContentSize(window.id);
+
+    const permissions = await ext.runtime.getPermissions();
+    const persistent =
+      (permissions["websessions"] ?? {})["create.persistent"]?.granted ?? false;
+
+    websession = await ext.websessions.create({
+      partition: title,
+      persistent,
+      global: false,
+    });
+    webview = await ext.webviews.create({
+      window,
+      websession,
+      autoResize: { horizontal: true, vertical: true },
+      bounds: { ...contentSize, x: 0, y: 0 },
+    });
+
+    await ext.webviews.loadFile(webview.id, "index.html");
+    // await ext.webviews.openDevTools(webview.id, {
+    //   mode: "detach",
+    //   activate: true,
+    // });
+
+    await ext.windows.focus(window.id);
+    await ext.webviews.focus(webview.id);
+
+    instance = {
+      tabId: tab.id,
+      windowId: window.id,
+      websessionId: websession.id,
+      webviewId: webview.id,
+    };
+  } catch (error) {
+    console.error("ext.runtime.onExtensionClick", JSON.stringify(error));
+
+    if (window) await ext.windows.remove(window.id);
+    if (tab) await ext.tabs.remove(tab.id);
+    if (websession) await ext.websessions.remove(websession.id);
+    if (webview) await ext.webviews.remove(webview.id);
+  }
+});
+
+ext.tabs.onClicked.addListener(async () => {
+  try {
+    await focusInstance();
+  } catch (error) {
+    console.log(error, "ext.tabs.onClicked");
+  }
+});
+
+ext.tabs.onClickedMute.addListener(async () => {
+  try {
+    if (instance) {
+      const muted = await ext.webviews.isAudioMuted(instance.webviewId);
+      await ext.webviews.setAudioMuted(instance.webviewId, !muted);
+      await ext.tabs.update(instance.tabId, { muted: !muted });
+    }
+  } catch (error) {
+    console.log(error, "ext.tabs.onClickedMute");
+  }
+});
+
+ext.tabs.onClickedClose.addListener(async () => {
+  try {
+    await destroyInstance();
+  } catch (error) {
+    console.log(error, "ext.tabs.onClickedClose");
+  }
+});
+
+ext.tabs.onRemoved.addListener(async () => {
+  try {
+    await destroyInstance();
+  } catch (error) {
+    console.log(error, "ext.tabs.onRemoved");
+  }
+});
+
+ext.windows.onClosed.addListener(async () => {
+  try {
+    await destroyInstance();
+  } catch (error) {
+    console.log(error, "ext.windows.onClosed");
+  }
+});
+
+ext.windows.onRemoved.addListener(async () => {
+  try {
+    await destroyInstance();
+  } catch (error) {
+    console.log(error, "ext.windows.onRemoved");
+  }
+});
+
+// ext.windows.onUpdatedDarkMode.addListener(async (event, details) => {
+//   try {
+//     await ext.windows.update(event.id, {
+//       icon: details.enabled ? "./assets/128.png" : "./assets/128-dark.png",
+//     });
+//   } catch (error) {
+//     console.log(error, "ext.windows.onUpdatedDarkMode");
+//   }
+// });
